79b8affcee4f5e9b61995ff9ada56b39a0f40118
milemu23 <mlmunksgard@fullsail.edu>
2017-03-23 21:18:55 -0400

    add conditional .env

--- src/models/db.js
+++ src/models/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 // load variables from .env file
-require('dotenv').config();
+if(!process.env.NODE_ENV) require('dotenv').config();
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
